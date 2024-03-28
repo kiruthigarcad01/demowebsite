989190d28ef844da5d2f1509e83a8d319304b185
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-03-28 18:01:07 +0530

    aded registration page

--- demoweb/Controllers/HomeController.cs
+++ demoweb/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Searchbar()
+    {
+        return View();
+    }
+
 
     public IActionResult Aboutus()
     {
